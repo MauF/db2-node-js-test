c88463691b325e608f4063c3fb9e63524252f983
MauF <maurizio.fassone@gmail.com>
2015-08-24 09:35:32 +0100

    added the parameters parser

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 var ibmdb = require('ibm_db');
+var argv = require('minimist')(process.argv.slice(2));
 
-var myhost = "localhost";
-var dbname = "TEST";
-var db2user = "db2inst1";
-var password = "db2inst1";
-var dbport = "50000";
+var dbhost = argv.dbhost;
+var dbname = argv.dbname;
+var db2user = argv.db2user;
+var password = argv.password;
+var dbport = argv.dbport;
+var query = argv.query;
 
+var usage = "USAGE: \n--dbhost=<db2-server-ip> --dbname=<db2-name> --db2user=<db2user> --password=<password> --dbport=<db2-server-port> --query=<query-to-execute>"
 
-ibmdb.open("DRIVER={DB2};DATABASE="+dbname+";HOSTNAME="+myhost+";UID="+db2user+";PWD="+password+";PORT="+dbport+";PROTOCOL=TCPIP", function (err,conn) {
-  if (err) return console.log(err);
+if (dbhost && dbname && db2user && password && dbport && query) {
+    ibmdb.open("DRIVER={DB2};DATABASE=" + dbname + ";HOSTNAME=" + dbhost + ";UID=" + db2user + ";PWD=" + password + ";PORT=" + dbport + ";PROTOCOL=TCPIP", function(err, conn) {
+        if (err) return console.log(err);
 
-  conn.query('select 1 from sysibm.sysdummy1', function (err, data) {
-    if (err) console.log(err);
-    else console.log(data);
+        conn.query('SELECT * FROM MDLR_CORE_CLUSTERS', function(err, data) {
+            if (err) console.log(err);
+            else console.log(data);
 
-    conn.close(function () {
-      console.log('done');
+            conn.close(function() {
+                console.log('done');
+            });
+        });
     });
-  });
-});
+} else {
+	console.info(usage);
+}
